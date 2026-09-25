ce010e0304835e5990ae94fa0df46326965bde5c
danzori <main.danzori@gmail.com>
2026-09-25 12:28:03 +0300

    refactor(config): drop unused health settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,19 +9,13 @@ type Config struct {
 	ServiceName     string        `validate:"required" yaml:"service_name"`
 	ShutdownTimeout time.Duration `validate:"required" yaml:"shutdown_timeout"`
 
-	GRPC   GRPC   `yaml:"grpc"`
-	Health Health `yaml:"health"`
-	Log    Log    `yaml:"log"`
+	GRPC GRPC `yaml:"grpc"`
+	Log  Log  `yaml:"log"`
 }
 
 type GRPC struct {
 	Addr       string `validate:"required" yaml:"addr"`
 	Reflection bool   `yaml:"reflection"`
-}
-
-type Health struct {
-	Period  time.Duration `validate:"required" yaml:"period"`
-	Timeout time.Duration `validate:"required" yaml:"timeout"`
 }
 
 type Log struct {
